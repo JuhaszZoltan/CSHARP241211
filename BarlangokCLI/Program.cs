c1dce2a40d5c4843a9f118112ed130d2afb21aca
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-12-11 09:26:16 +0100

    Add project files.

---
+++ BarlangokCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using BarlangokCLI;
+using System.Text;
+
+const string eleresiUt = "C:\\Users\\juhaszz\\source\\repos\\CSHARP241211\\BarlangokCLI\\src\\barlangok.txt";
+
+List<Barlang> barlangok = [];
+
+using StreamReader sr = new(eleresiUt, Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) barlangok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"4. feladat: Barlangok száma: {barlangok.Count}");
